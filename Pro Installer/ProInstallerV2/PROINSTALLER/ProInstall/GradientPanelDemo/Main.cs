f61a938f2ffcd2f08c1e0324796b3332e6fef3ac
Storm <78948929+Mysa99@users.noreply.github.com>
2021-03-20 13:02:58 +0100

    Add files via upload

---
+++ Pro Installer/ProInstallerV2/PROINSTALLER/ProInstall/GradientPanelDemo/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradientPanelDemo
+{
+    public partial class Main : Form
+    {
+        
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void gradientButton1_Click(object sender, EventArgs e)
+        {
+            new Fortnite_14().Show();
+        }
+
+       
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Process.Start("https://discord.gg/UBMDXnZcM8");
+        }
+
+        private void gradientButton2_Click(object sender, EventArgs e)
+        {
+            new fortnite18().Show();
+        }
+    }
+}
